366656bed8dc0a8475ca721429531f2990b9e03e
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2012-03-22 17:39:10 +0000

    util_spec

--- spec/mdquery/util_spec.rb
+++ spec/mdquery/util_spec.rb
@@ -1,8 +1,16 @@
 require File.expand_path('../../spec_helper', __FILE__)
 require 'mdquery/util'
 
-describe "MDQuery::Util" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe MDQuery::Util do
+  describe "assign_attributes" do
+    it "should assign attributes" do
+      attrs = {:foo=>100, :bar=>"hi"}
+      instance = Object.new
+
+      mock(instance).instance_variable_set("@foo",100)
+      mock(instance).instance_variable_set("@bar", "hi")
+
+      MDQuery::Util.assign_attributes(instance, attrs)
+    end
   end
 end
